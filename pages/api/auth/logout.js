7bf0f7b64be66dc62920d4cca2ea38510d875421
deniskovalchuk55-ux <deniskovalchuk55@gmail.com>
2026-07-15 20:37:20 +0300

    Delete pages directory

--- pages/api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie } from "../../../lib/auth";
-
-export default async function handler(req, res) {
-  res.setHeader("Set-Cookie", clearSessionCookie());
-  return res.status(200).json({ ok: true });
-}
